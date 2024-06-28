79532bb4528d466ee6cce9ca6f4c05e0e64906d6
alex <alexandersegerev@gmail.com>
2024-06-28 09:04:55 -0400

    Testing to make sure it works - WILL UPDATE ASAP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(){
+  return(
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Watch all your favorite friends on Y12!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn English with Me!
+        </a>
+      </header>
+    </div>
+  )
+}
+
+//------------------------------------------------------------------------------------------------------------------
+function AppOld() {
   return (
     <div className="App">
       <header className="App-header">
