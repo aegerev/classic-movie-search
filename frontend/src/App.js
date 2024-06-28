2c6d2039805cd5590f940d5cf31ef641269a0937
alex <alexandersegerev@gmail.com>
2024-06-28 09:17:43 -0400

    Added new components for the project to work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
+import {Switch, Route, Link} from 'react-router-dom'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import MoviesList from './components/movies-list';
+import Movie from './components/movie';
+import Login from './components/login';
+
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 
 function App(){
   return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
          Watch all your favorite friends on Y12!
         </p>
@@ -27,7 +34,7 @@ function AppOld() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ frontend/src/components/login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login(){
+    return(
+        <div className='App'>
+       login now
+        </div>
+    );
+}
+
+export default Login;
